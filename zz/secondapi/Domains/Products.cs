97132b0a3f4f44f43b0a9f30c4f51d583d5d6d5f
joao-henrike <133373332+joao-henrike@users.noreply.github.com>
2024-08-07 14:30:42 -0300

    Add files via upload

---
+++ zz/secondapi/Domains/Products.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace secondapi.Domains
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Id { get; set; }
+
+
+        [BsonElement("price")]
+        public string Id 
+        
+
+    }
+}
+
+
+//using MongoDB.Bson;
+//using MongoDB.Bson.Serialization.Attributes;
+
+//namespace minimalAPIMongo.Product
+//{
+    //public class Product
+    //{
+        //[BsonId]
+        //[BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        //public string Id { get; set; }
+
+        //[BsonElement("name")]
+        //public string Name { get; set; }
+
+        //[BsonElement("price")]
+        //public decimal Price { get; set; }
+
+        //public Dictionary<string, string> AddtionalAttributes { get; set; }
+
+       // public Product()
+        //{
+            //AddtionalAttributes = new Dictionary<string, string>();
+        //}
+   // }
+//}
